0b72c05a5abb8f0e8e8b77be224d07cfbaca25de
NarkoV <narkov@yahoo.com>
2012-03-31 20:36:01 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongodocs::Application.config.secret_token = 'b3d67c1dae5b2a68f8c378a9b80c59f1729718aba81727b2494052b4def113fac5c8e0e1aa22de84872b9c8a6be5411906ee41bf5436cdf4e7c647d5822d0d54'
